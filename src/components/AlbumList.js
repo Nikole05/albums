3c892a7b128a4b8a0f9c999e3c20dd90a685dfff
Nico <nico@Nicos-MacBook-Pro.local>
2018-12-31 00:54:44 +0200

    changet AlbumList comp class based comp

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
+class AlbumList extends Component {
+    render() {
     return (
         <View>
             <Text>Album List!!!!</Text>
         </View>
     );
-};
+  }
+}
 
 export default AlbumList;
